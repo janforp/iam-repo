1efdf8680095a597cb9b9aa2cbeb43f4017b6327
janforp <janforp@163.com>
2017-05-21 20:15:08 +0800

    自定义校验规则

--- src/main/java/com/zbmatsu/iam/repository/IRelationShipRepository.java
+++ src/main/java/com/zbmatsu/iam/repository/IRelationShipRepository.java
@@ -18,6 +18,12 @@ public interface IRelationShipRepository extends TypedIdCassandraRepository<Rela
     @Query("select * from relationship where user_id = :userId and target_id = :targetId ALLOW FILTERING")
     List<RelationShip> getByUserIdAndTargetId(@Param("userId") String userId, @Param("targetId") String targetId);
 
+    /**
+     * 只要where后面的不是主键就要加上ALLOW FILTERING
+     * @param userId
+     * @param targetType
+     * @return
+     */
     @Query("select * from relationship where user_id = :userId and target_type = :targetType ALLOW FILTERING")
     List<RelationShip> getByUserIdAndTargetType(@Param("userId") String userId, @Param("targetType")String targetType);
 }
